0ae17322b2e511d775531585fc2e3c53a833439e
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-05-21 14:38:35 -0400

    Clientes

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -6,8 +6,5 @@ public class Context : DbContext
     public DbSet<Prioridades> Prioridades { get; set; }
 
     public Context(DbContextOptions<Context> options) : base(options)
-    {
-        
-    }
-
+    {}
 }
