438338d7235c2f83be8fc9f5d10ae90a18054545
Kissland Baker <Kissland_baker@ucne.edu.do>
2023-05-28 05:14:35 -0400

    Se arreglo un problemita con el asunto del cliente

--- Models/Clientes.cs
+++ Models/Clientes.cs
@@ -7,26 +7,21 @@ public class Clientes{
     public int ClienteId { get; set; }
 
     [Required(ErrorMessage = "El campo nombre es obligatorio")]
-    [StringLength(maximumLength: 15, MinimumLength = 10)]
+
     public string Nombre { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "El campo telefono es obligatorio")]
-    [StringLength(maximumLength: 15, MinimumLength = 10)]
     public string? Telefono { get; set; }
 
     [Required(ErrorMessage = "El campo celular es obligatorio")]
-    [StringLength(maximumLength: 15, MinimumLength = 10)]
     public string? Celular { get; set; }
 
     [Required(ErrorMessage = "El campo rnc es obligatorio")]
-    [StringLength(maximumLength: 15, MinimumLength = 10)]
     public string RNC { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "El campo email es obligatorio")]
-    [StringLength(maximumLength: 30, MinimumLength = 15)]
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "El campo direccion es obligatorio")]
-    [StringLength(maximumLength: 30, MinimumLength = 15)]
     public string Direccion { get; set; } = string.Empty;
 }
